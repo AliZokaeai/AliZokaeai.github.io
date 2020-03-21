4d8cbdfefe298d85e7d270643fb224e399df2af8
AliZokaea <alizokaeaidehaghani@gmail.com>
2020-03-21 12:05:41 +0430

    build

---
+++ precache-manifest.bd225b5132f73a67472d842a545ab3f6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35eff52c994b57b420108b7beb6bd827",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cb956b3730e56284249",
+    "url": "/static/css/main.467f8904.chunk.css"
+  },
+  {
+    "revision": "25667b638d394d95bd0a",
+    "url": "/static/js/2.0827e008.chunk.js"
+  },
+  {
+    "revision": "64fa559f9933e20ffd40833bd0e5efa2",
+    "url": "/static/js/2.0827e008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cb956b3730e56284249",
+    "url": "/static/js/main.2cff5cfd.chunk.js"
+  },
+  {
+    "revision": "68b3ff9fd67018062622",
+    "url": "/static/js/runtime-main.34da51bc.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/static/media/slide_two.3a3ffd36.jpg"
+  }
+]);
